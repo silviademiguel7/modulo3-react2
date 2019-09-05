77441750e1200d6f91563ab33fcd3a85f905558d
silvia <silviademiguel7@gmail.com>
2019-09-05 12:54:39 +0200

    ejercicio 4 prepare to do

---
+++ ej4/proyect-react4/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './App.css';
+import CatList from './components/CatList';
+
+function App() {
+  return (
+    <div className="App">
+      <CatList/>
+    </div>
+  );
+}
+
+export default App;
