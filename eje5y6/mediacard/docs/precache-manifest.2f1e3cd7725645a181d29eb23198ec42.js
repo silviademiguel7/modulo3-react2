ddfe9c5abb96915846c1da0f8bf0e6816542f608
silvia <silviademiguel7@gmail.com>
2019-09-06 20:33:10 +0200

    add docs folder

---
+++ eje5y6/mediacard/docs/precache-manifest.2f1e3cd7725645a181d29eb23198ec42.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a0fe0c5a0243cc9ea73068e9334d6fe",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0053447f14cccaed86fe",
+    "url": "./static/css/main.898e5007.chunk.css"
+  },
+  {
+    "revision": "117081121626afa295ef",
+    "url": "./static/js/2.3f114a7f.chunk.js"
+  },
+  {
+    "revision": "0053447f14cccaed86fe",
+    "url": "./static/js/main.3cb5c828.chunk.js"
+  },
+  {
+    "revision": "1484420eb00ab49792e4",
+    "url": "./static/js/runtime~main.7fcb2657.js"
+  },
+  {
+    "revision": "a6a2fc1ecf6b58d871c117e11f344bfb",
+    "url": "./static/media/heart-solid.a6a2fc1e.svg"
+  }
+]);
---
+++ eje5y6/mediacard/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import MediaList from './MediaList';
+
+class App extends React.Component{
+    
+    render (){
+        return (
+           <MediaList />
+        )
+    }
+}
+
+export default App;
